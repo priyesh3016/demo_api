0dd7e7616254e9b9bd559adbe8c66d154d4ecd70
yash <patil998707@gmail.com>
2026-05-11 17:59:21 +0530

    pojo classes added

--- src/pojo/Courses.java
+++ src/pojo/Courses.java
@@ -1,5 +1,26 @@
 package pojo;
 
 public class Courses {
-
+	
+	private String webAutomation ;
+	public String getWebAutomation() {
+		return webAutomation;
+	}
+	public void setWebAutomation(String webAutomation) {
+		this.webAutomation = webAutomation;
+	}
+	public String getApi() {
+		return Api;
+	}
+	public void setApi(String api) {
+		Api = api;
+	}
+	public String getMobile() {
+		return mobile;
+	}
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+	private String Api ;
+	private String mobile ;
 }
--- src/pojo/GetCourse.java
+++ src/pojo/GetCourse.java
@@ -1,5 +1,52 @@
 package pojo;
 
 public class GetCourse {
+	
+	
+	private String URL ;
+	private String services ;
+	private String expertise ;
+	private String Course ;
+	private String instructor ;
+	private String Linkedin ;
+	
+	
+	public String getURL() {
+		return URL;
+	}
+	public void setURL(String uRL) {
+		URL = uRL;
+	}
+	public String getServices() {
+		return services;
+	}
+	public void setServices(String services) {
+		this.services = services;
+	}
+	public String getExpertise() {
+		return expertise;
+	}
+	public void setExpertise(String expertise) {
+		this.expertise = expertise;
+	}
+	public String getCourse() {
+		return Course;
+	}
+	public void setCourse(String course) {
+		Course = course;
+	}
+	public String getInstructor() {
+		return instructor;
+	}
+	public void setInstructor(String instructor) {
+		this.instructor = instructor;
+	}
+	public String getLinkedin() {
+		return Linkedin;
+	}
+	public void setLinkedin(String linkedin) {
+		Linkedin = linkedin;
+	}
+
 
 }
